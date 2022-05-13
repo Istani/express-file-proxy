e4b6f6cd4c72fe2a04e551fa9286f1e6410bef73
YoursMine-Server <sascha.u.kaufmann@googlemail.com>
2022-05-13 13:27:00 +0200

    feat: express proxy module?

---
+++ index.js
@@ -0,0 +1,38 @@
+const package_info = require('./package.json');
+var software = package_info.name + ' (V ' + package_info.version + ')';
+
+const http = require('http');
+const https = require('https');
+
+async function middelware(req, res, next) {
+  /*
+    // Needs!
+    const cors = require("cors");
+    app.use(
+      cors({
+        origin: '*'
+      })
+    );
+  */
+  
+  if (typeof req.body.url != "undefined") {
+    var conn=null;
+    if (req.body.url.replace('https://','')!=req.body.url) {
+      conn=https;
+    } else {
+      conn=http;
+    }
+    await conn.get(req.body.url, {headers: { 'User-Agent': package_info.name+'; '+req.get('User-Agent') }}, function async (response) {
+      response.on('data', async (data) => {
+        res.send(data);
+      });
+    }).on('error', function (e) {
+      res.send(e);  
+    });
+  } else {
+    res.send({'message':'missing url'});
+  }
+  //next();
+}
+
+module.exports = middelware;
---
+++ test.js
@@ -0,0 +1,50 @@
+var code_file = __filename.replace(__dirname + '/', '');
+const package_info = require('./package.json');
+var software =
+  package_info.name + ' (V ' + package_info.version + ') - ' + code_file;
+console.log(software);
+console.log('='.repeat(software.length));
+console.log();
+
+const debug = require('../debug');
+process.chdir(__dirname);
+
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
+app.use(
+  cors({
+    origin: '*'
+  })
+);
+
+const test = require('./index.js');
+
+app.use(express.json());
+
+/*
+app.get('/:origin', (req, res, next) => {
+  if (typeof req.params.origin != 'undefined') {
+    switch (req.params.origin) {
+      case 'test':
+        req.params.url="http://example.org/test.json";
+        break;
+    }
+  }
+  test(req, res, next);
+});
+*/
+
+//app.get('*', test);
+app.post('*', (req, res, next) => {
+  console.log('Get Post: ',req.body);
+  test(req, res, next);
+});
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(4001, () => {
+  debug.log('HTTP Server running! Port: ' + 4001, package_info.name + '-APP');
+});
